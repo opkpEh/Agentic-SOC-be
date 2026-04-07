122f93f8ba0931fa0e1e5b461a6c55829a4e0318
Kushagra Pandey <56930383+opkpEh@users.noreply.github.com>
2026-04-07 18:33:17 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-WEBHOOK_URL = "https://devcohort.space/webhook-test/ae6e5156-98a0-4fc4-b250-1b4db6e9f109"
+WEBHOOK_URL = "https://n8n.devcohort.space/webhook-test/ae6e5156-98a0-4fc4-b250-1b4db6e9f109"
 
 TRUSTED_IPS = {
     "10.10.10.5",
